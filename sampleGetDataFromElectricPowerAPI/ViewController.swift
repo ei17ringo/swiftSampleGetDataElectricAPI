a3acab8aa50de43ca2647af88a7e5736f45c83c1
ei17ringo <ei17ringo@gmail.com>
2016-02-03 20:50:00 +0800

    commit init

--- sampleGetDataFromElectricPowerAPI/ViewController.swift
+++ sampleGetDataFromElectricPowerAPI/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myDatePicker.datePickerMode = UIDatePickerMode.Date
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapBtn(sender: UIButton) {
+
+        let df = NSDateFormatter()
+        df.dateFormat = "yyyy/MM/dd"
+        var dateStr = df.stringFromDate(myDatePicker.date)
+        
+        var url = NSURL(string: "http://tepco-usage-api.appspot.com/\(dateStr)/0.json")
+        var request = NSURLRequest(URL:url!)
+        var jsondata = (try! NSURLConnection.sendSynchronousRequest(request, returningResponse: nil))
+        let jsonDictionary = (try! NSJSONSerialization.JSONObjectWithData(jsondata, options: [])) as! NSDictionary
+        for(key, data) in jsonDictionary{
+            print("\(key)=\(data)")
+        }
+        
+    }
 
 }
 
